f4a1cd069062ad25a367f33c0a1892ff0198bfab
va9iff <va9iff@gmail.com>
2021-06-23 20:59:03 +0400

    complete look

--- Docronym.js
+++ Docronym.js
@@ -1,8 +1,9 @@
 content = document.querySelector("#content");
 
-document.querySelectorAll("h3").forEach((element) => {
-  element.addEventListener(
-    "click",
-    (e) => (content.innerHTML = element.innerHTML)
-  );
+document.querySelectorAll("button").forEach((element) => {
+  let cont = element.innerHTML;
+  for (let times = 0; times < 9; times++) {
+    cont = cont + cont;
+  }
+  element.addEventListener("click", (e) => (content.innerHTML = cont));
 });
