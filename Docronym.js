42ce3dc221c1f88fc62d339c79d6249adb13b13c
va9iff <va9iff@gmail.com>
2021-07-03 22:55:02 +0400

    framework is ready

--- Docronym.js
+++ Docronym.js
@@ -1,9 +1,27 @@
 content = document.querySelector("#content");
+Dlist = document.querySelector("#Dlist");
 
-document.querySelectorAll("button").forEach((element) => {
-  let cont = element.innerHTML;
-  for (let times = 0; times < 9; times++) {
-    cont = cont + cont;
+$.getJSON(
+  document.URL.substr(0, document.URL.lastIndexOf("/")) + "/Dposts",
+  (data) => {
+    // console.log(data); //["doc1.jpg", "doc2.jpg", "doc3.jpg"]
+    var allPosts = data;
+    console.log(allPosts.length);
+    for (let index = 0; index < allPosts.length; index++) {
+      let P = allPosts[index];
+      console.log("asfjd");
+      element = document.createElement("button");
+      element.innerHTML = P;
+      element.addEventListener("click", (e) => {
+        // content.innerHTML = element.innerHTML + "VVVVVVVVVVVV";
+        fetch("./Dposts/" + P).then((r) => {
+          r.text().then((d) => {
+            // let CONTENT = d;
+            content.innerHTML = d;
+          });
+        });
+      });
+      Dlist.appendChild(element);
+    }
   }
-  element.addEventListener("click", (e) => (content.innerHTML = cont));
-});
+);
